c736e0a7c2f69b264c91489b514e92412f342dc0
William SIMON--VEZO <william.simonvezo@gmail.com>
2018-02-27 00:46:18 +0100

    Added basic sqlite3 integration
    - Added basic sqlite3 integration.
    - Added sqlite file gitignore.
    - Added module "bookshelf" for models definition.

--- express/express.js
+++ express/express.js
@@ -10,6 +10,71 @@ app.listen(3000, function(){
   console.info('Listening on port 3000...');
 });
 
+const knex = require('knex')({
+  'client' : 'sqlite3',
+  'connection' : {
+    'filename' : 'data.sqlite'
+  },
+  'useNullAsDefault' : true
+});
+
+const bookshelf = require('bookshelf')(knex);
+
+// Create table rssFeeds if it does not exist.
+knex.schema.hasTable('rssFeeds').then(function(exists){
+  if(!exists){
+    return knex.schema.createTable('rssFeeds', function(table){
+      table.increments('id');
+      table.string('url', 250);
+      table.text('xml', 'longtext');
+    }).then(function(){
+      console.info('Table "rssFeeds" created.');
+    }).catch(function(err){
+      console.info('Error : table "rssFeeds" creation failed.');
+      console.info(err);
+    });
+  }
+});
+
+// RssFeed model
+const RssFeed = bookshelf.Model.extend({
+  'tableName' : 'rssFeeds'
+}, {
+  'create' : function(data, options){
+    return this.forge(data).save(null, options);
+  },
+  'findById' : function(id, options){
+    return this.findOne({[this.prototype.idAttribute] : id}, options);
+  },
+  'findByUrl' : function(url){
+    return this.forge().query({'where' : {'url' : url}}).fetch();
+  },
+  'findAll' : function(filter, options){
+    return this.forge().where(filter).fetchAll(options);
+  },
+  'findOne' : function(query, options){
+    return this.forge(query).fetch(options);
+  },
+  'destroy' : function(options){
+    return this.forge({[this.prototype.idAttribute] : options.id}).destroy(options);
+  }
+});
+
+// Test find (by id)
+RssFeed.findById(1).then(function(rssFeed){
+  console.info(rssFeed);
+});
+
+// Test create
+const newRssFeed = {
+  'url' : 'https://www.judgehype.com/nouvelles.xml'
+};
+RssFeed.create(newRssFeed).then(function(rssFeed){
+  console.info(rssFeed);
+});
+
+
+
 // Serve views folder (dist)
 app.use('', express.static(path.join(__dirname, '../dist')));
 
