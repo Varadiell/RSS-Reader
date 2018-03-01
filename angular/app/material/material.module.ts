7e15f938b4d67354850115cdbca53f295512c634
William SIMON--VEZO <william.simonvezo@gmail.com>
2018-03-01 19:29:32 +0100

    Added Font-Awesome (AngularFontAwesomeModule)
    - Added Font-Awesome (via AngularFontAwesomeModule).

--- angular/app/material/material.module.ts
+++ angular/app/material/material.module.ts
@@ -8,9 +8,11 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 @NgModule({
   exports: [
+    AngularFontAwesomeModule,
     MatButtonModule,
     MatCardModule,
     MatDividerModule,
