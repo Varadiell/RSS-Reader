5cf600e73ce8a8b55ec7ea5fb267472d52dbb013
William SIMON--VEZO <william.simonvezo@gmail.com>
2018-03-01 17:26:24 +0100

    Added MatCardModule
    - Added MatCardModule.

--- angular/app/material/material.module.ts
+++ angular/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
@@ -9,6 +10,7 @@ import { MatMenuModule } from '@angular/material/menu';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatFormFieldModule,
     MatInputModule,
     MatMenuModule
