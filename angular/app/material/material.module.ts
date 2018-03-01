3eed78c96e42a462308efbd2a0258cfa553fae44
William SIMON--VEZO <william.simonvezo@gmail.com>
2018-03-01 18:14:21 +0100

    Added MatListModule
    - Added MatListModule.

--- angular/app/material/material.module.ts
+++ angular/app/material/material.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   exports: [
     MatButtonModule,
     MatCardModule,
+    MatDividerModule,
     MatFormFieldModule,
     MatInputModule,
+    MatListModule,
     MatMenuModule
   ],
   imports: [
