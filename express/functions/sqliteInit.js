2819efdfc51871b98e2bd73dcc8446e511718139
William SIMON--VEZO <william.simonvezo@gmail.com>
2018-03-02 10:39:32 +0100

    Updated sqliteInit for table rssFeeds & rssNews
    - Updated sqliteInit for table rssFeeds.
    - Created sqliteInit for table rssNews.
    - Added model rssNews (bookshelf).

--- express/functions/sqliteInit.js
+++ express/functions/sqliteInit.js
@@ -7,11 +7,32 @@ knex.schema.hasTable('rssFeeds').then(function(exists){
   if(exists) return;
   return knex.schema.createTable('rssFeeds', function(table){
     table.increments('id').primary();
+    table.string('description', 1000);
+    table.string('image', 250);
+    table.string('link', 250);
     table.string('url', 250);
   }).then(function(){
     console.info('Table "rssFeeds" created.');
   }).catch(function(err){
     console.info('Error : table "rssFeeds" creation failed.');
+    console.info(err);
+  });
+});
+
+// Create table rssNews if it does not exist.
+knex.schema.hasTable('rssNews').then(function(exists){
+  if(exists) return;
+  return knex.schema.createTable('rssNews', function(table){
+    table.increments('id').primary();
+    table.integer('rssFeedId');
+    table.string('description', 1000);
+    table.string('link', 250);
+    table.string('pubDate', 100);
+    table.string('title', 150);
+  }).then(function(){
+    console.info('Table "rssNews" created.');
+  }).catch(function(err){
+    console.info('Error : table "rssNews" creation failed.');
     console.info(err);
   });
 });
---
+++ express/models/rssNews.js
@@ -0,0 +1,33 @@
+// Bookshelf
+const bookshelf = rootRequire('express/config/bookshelf');
+
+
+
+// RssNews model
+const RssNews = bookshelf.Model.extend({
+  'tableName' : 'rssNews'
+}, {
+  'create' : function(rssNews){
+    return this.forge(rssNews).save();
+  },
+  'destroy' : function(id){
+    return this.forge({'id' : id}).destroy();
+  },
+  'findAll' : function(filter){
+    return this.forge(filter).fetchAll();
+  },
+  'findById' : function(id){
+    return this.forge({'id' : id}).fetch();
+  },
+  'findByRssFeedId' : function(id){
+    return this.forge({'rssFeedId' : id}).fetch();
+  },
+  'findOne' : function(query){
+    return this.forge(query).fetch();
+  }
+});
+
+
+
+// Exports
+module.exports = RssNews;
