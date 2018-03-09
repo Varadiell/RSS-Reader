4435bc0ba9b4a06fac387ba070065291a20a4e35
William SIMON--VEZO <william.simonvezo@gmail.com>
2018-03-09 15:12:37 +0100

    Added rss feed icons display
    - Added rss feed icons display.

--- angular/app/modules/material.module.ts
+++ angular/app/modules/material.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
@@ -20,6 +21,7 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
     MatCardModule,
     MatDividerModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
     MatListModule,
     MatMenuModule,
