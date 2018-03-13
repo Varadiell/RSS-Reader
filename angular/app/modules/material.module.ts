b2c33510a9f6652340c5d93e13f25de3f0251d20
William SIMON--VEZO <william.simonvezo@gmail.com>
2018-03-13 13:45:26 +0100

    Fixed use of font-awesome icons
    - Fixed use of font-awesome icons.
    - Removed useless angular-font-awesome module.

--- angular/app/modules/material.module.ts
+++ angular/app/modules/material.module.ts
@@ -6,7 +6,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
@@ -37,6 +37,17 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome';
   ],
   imports: [
     CommonModule
+  ],
+  providers: [
+    MatIconRegistry
   ]
 })
-export class MaterialModule { }
+export class MaterialModule {
+
+  constructor(
+    public matIconRegistry: MatIconRegistry
+  ) {
+    matIconRegistry.registerFontClassAlias('fontawesome', 'fa');
+  }
+
+}
